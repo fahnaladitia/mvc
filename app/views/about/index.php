687e5fdf24e4f4c3a0f843ab1cc42c450130d3bd
fahnaladitia <fahnaladitia07@gmail.com>
2021-02-15 16:02:20 +0800

    Menambahkan Bootstrap dan assets Pada Project

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
- <h1>About Me</h1>
+<div class="container">
+ <h1 class="mt-4">About Me</h1>
+ <img src="<?= BASEURL; ?>/img/profile1.png" width="200" class="rounded-circle shadow" alt="">
  <p>Halo, nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
