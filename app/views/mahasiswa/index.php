2df7cee32e1d3de9c606913f8f1ff716f44e7b66
fahnaladitia <fahnaladitia07@gmail.com>
2021-02-15 19:27:36 +0800

    Menambah Fitur Insert Data Mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,11 @@
- <div class="container mt-5">
+ <div class="container mt-3">
   <div class="row">
    <div class="col-6">
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+      Tambah Data Mahasiswa
+    </button>
+    <br><br>
     <h3>Daftar Mahasiswa</h3>
     
     <ul class="list-group">
@@ -14,3 +19,47 @@
    </div>
   </div>
  </div>
+
+ <!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+          <div class="form-group">
+           <label for="nama">Nama : </label>
+           <input type="text" class="form-control" maxlength="50" name="nama" id="nama">
+          </div>
+          <div class="form-group">
+           <label for="nim">NIM : </label>
+           <input type="number" class="form-control" maxlength="13" name="nim" id="nim">
+          </div>
+          <div class="form-group">
+           <label for="email">Email : </label>
+           <input type="email" class="form-control" maxlength="50" name="email" id="email">
+          </div>
+          <div class="form-group">
+           <label for="jurusan">Jurusan</label>
+           <select class="form-control" id="jurusan" name="jurusan">
+             <option value="Teknik Informatika">Teknik Infomatika</option>
+             <option value="Teknik Mesin">Teknik Mesin</option>
+             <option value="Teknik Alat Berat">Teknik Alat Berat</option>
+             <option value="Management">Management</option>
+             <option value="Keperawatan">Keperawatan</option>
+             <option value="Farmasi">Farmasi</option>
+           </select>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+         </form>
+      </div>
+    </div>
+  </div>
+</div>
